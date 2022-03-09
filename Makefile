dacbacbfc2476f67f5448107494cc8e8cf70b159
brian978 <brian978@users.noreply.github.com>
2022-03-09 08:29:18 +0200

    Updated the build process to also build dev images

--- Makefile
+++ Makefile
@@ -1,16 +1,21 @@
-.DEFAULT_GOAL := 74
+.DEFAULT_GOAL := 81
+
+env=prod
 
 73:
-	sh ./build.sh 7.3
+	@sh ./build.sh 7.3
 
 74:
-	sh ./build.sh 7.4
+	@sh ./build.sh 7.4
 
 80:
-	sh ./build.sh 8.0
+	@sh ./build.sh 8.0
 
+81:
+	@sh ./build.sh 8.1 $(env)
 
 all:
 	sh ./build.sh 7.3
 	sh ./build.sh 7.4
 	sh ./build.sh 8.0
+	sh ./build.sh 8.1 $(env)
--- build.sh
+++ build.sh
@@ -2,6 +2,36 @@
 
 DIR=$(pwd)
 VERSION=$1
+ENV=$2
 
-docker build -t brian978/php-fpm:$VERSION $DIR/$VERSION
-docker push brian978/php-fpm:$VERSION
+SUFFIX=''
+
+function build_image () {
+    env=$1
+    version=$2
+    suffix=$3
+
+    # Build and PUSH the image
+    echo "Building the $env image"
+    docker build -t brian978/php-fpm:$version$suffix "$DIR/$version/$env"
+
+    echo "Pushing the $env image"
+    docker push brian978/php-fpm:$version$suffix
+}
+
+if [ $ENV = 'dev' ]
+then
+    SUFFIX='-dev'
+
+    echo "Production image hash: $(docker images -q brian978/php-fpm:$VERSION 2> /dev/null)"
+
+    # Build the prod image first as the DEV one requires it
+    if [[ "$(docker images -q brian978/php-fpm:$VERSION 2> /dev/null)" == "" ]]
+    then
+        echo "Building the production image"
+        build_image 'prod' $VERSION $SUFFIX
+    fi
+fi
+
+# Build and PUSH the image
+build_image $ENV $VERSION $SUFFIX
